73af75979e1b50ee1a9ebea71ef0da12a707a61c
Annalise Murphy <nali247@gmail.com>
2020-04-01 16:25:40 -0700

    html and css set up

---
+++ app.js
@@ -0,0 +1,14 @@
+const radios = document.querySelectorAll('rock-paper-scissors');
+const playGameButton = document.getElementById('play-game-button');
+const gameResult = document.getElementById ('game-result');
+const resetButton = document.getElementById ('reset-button');
+/* span input needed */
+playGameButton.addEventListener('click', () => {
+    /* choose the selected play - rock - paper - scissors */
+    /* get a random selector option - between 3 */
+    /* calculate if the user one */
+    /* change state based off calc */
+    console.log(radios);
+
+
+});
