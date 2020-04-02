c52a8233970c0d425542ca4839037b4cad8d2010
Annalise Murphy <nali247@gmail.com>
2020-04-02 08:50:13 -0700

    fixing travis

--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,2 @@
 // IMPORT MODULES under test here:
-import compareResult from '../compareResult.js';
-// import example from '../src/example.js';
-
-const test = QUnit.test;
-
-test('time to test a function', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
-    const playerThrow = 'rock';
-    const computerThrow = 'paper';
-    const expected = 'lose';
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const result = compareResult(playerThrow, computerThrow);
-    //Assert
-    // Make assertions about what is expected valid result
-    assert.equal(result, expected);
-});
+import './myTest.test.js';
